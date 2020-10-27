183c7468988fbfd0e3d307032b6527eaefb024ed
Michał Hajduk <michal.hajduk@e3.pl>
2020-10-27 20:11:02 +0100

    repaired navigation

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React from 'react';
 import PageNav from '../PageNav/PageNav';
-
+import PropTypes from 'prop-types';
 
 const MainLayout = props => (
-      <div>{props.children}
-        <PageNav/>
-      </div>
-    )
+  <div>
+    <PageNav/>
+    {props.children}
+  </div>
+);
+
+MainLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 export default MainLayout;
+
